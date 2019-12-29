8e535a8d7474c847872bd089669cc0cecfc6af53
Sergii Khomenko <khomenko@brainscode.com>
2019-12-29 12:49:38 +0200

    Add local cache; big interactive altair map

--- readme.md
+++ readme.md
@@ -1,19 +1,28 @@
 # Places traveled
 
 It's hard to reach your goals if you do not have any.
-It's certainly nice to leave some space for serenity, but it might also mean that you do not really make constant progress
+On the other hand, it's certainly nice to leave some space for serenity, but it might also mean that you do not really make constant progress.
 
 The idea of this project is to have a set of scripts that allow revisiting visually traveled destinations. As the primary matter of keeping track of these, I used Foursquare/Swarm.
 
 ## Installation
 
 Export Foursquare token via env variable
+
 ```sh
 export FOURSQUARE=YOUR_TOKEN_HERE
 ```
+
 Or if executed from within the shell like Google Colab or running Jupyter
+
 ```
 %env FOURSQUARE=YOUR_TOKEN_HERE
+```
+
+## Running with [Streamlit](https://streamlit.io/s)
+
+```sh
+streamlit run streamlit/main.py
 ```
 
 ---
--- streamlit/main.py
+++ streamlit/main.py
@@ -10,10 +10,28 @@ from datetime import datetime
 from typing import List
 
 
-@st.cache()
+# @st.cache()
 def get_foursquare_data(foursquare_token):
+
+    # TODO: move it to another wrapper
+    # TODO: check the lifecycle of streamlit
+    cache_filename = 'foursquare.cache'
+    import os.path
+    import json
+
+    if os.path.exists(cache_filename):
+        print('loading from local cache')
+
+        with open(cache_filename) as cache_file:
+            cached = json.load(cache_file)
+
+            return cached
+    else:
+        print('loading not cached version')
+
     client = foursquare.Foursquare(access_token=foursquare_token)
     all_checkins = client.users.all_checkins()
+    # all_checkins = client.users.checkins()
 
     print(all_checkins)
     print("I hope we do not read this over and over")
@@ -26,7 +44,62 @@ def get_foursquare_data(foursquare_token):
                   'location': checkin['venue']['name']
                  } for checkin in all_checkins]
 
+    with open(cache_filename, 'w') as cache_file:
+        json.dump(processed, cache_file)
+
+        print("Just dumped file to cache")
+
     return processed
+
+def get_visited_map(checkins_df):
+    from vega_datasets import data
+
+    vega_countries = alt.topo_feature(data.world_110m.url, 'countries')
+
+    selection = alt.selection_multi(fields=['year'])
+    color = alt.condition(selection,
+                        alt.Color('year:N', legend=None),
+                        alt.value('lightgray'))
+
+    size = alt.condition(selection,
+                        alt.value(200),
+                        alt.value(10))
+
+    base = alt.Chart(vega_countries).mark_geoshape(
+        # fill='#666666',
+        # stroke='white'
+    ).properties(
+        # width=800,
+        # height=800
+    ).project('mercator')
+
+    points = alt.Chart(checkins_df).mark_circle(
+        opacity=0.5,
+        size=20
+    ).encode(
+        longitude='lng:Q',
+        latitude='lat:Q',
+        tooltip=['city', 'country', 'year'],
+        color=color,
+        size=size
+    )
+
+    legend = alt.Chart(checkins_df).mark_bar().encode(
+        y=alt.Y('year:N', axis=alt.Axis(orient='right')),
+        color=color
+    ).add_selection(
+        selection
+    )
+
+    country_list = alt.Chart(checkins_df).mark_bar(
+
+    ).transform_filter(
+        selection
+    ).encode(
+        y='country:N',
+    )
+
+    return (base + points) | legend | country_list
 
 
 def main():
@@ -41,7 +114,7 @@ def main():
     foursquare_data = get_foursquare_data(FOURSQUARE_TOKEN)
     checkins_df = pd.DataFrame.from_dict(foursquare_data)
 
-    st.markdown("The tracked data")
+    st.markdown("The tracked data with Foursquare")
     st.dataframe(checkins_df)
 
     MOST_LIMIT = 3
@@ -79,9 +152,11 @@ def main():
         color='country',
         tooltip=['country', 'location']
     )
-    st.markdown("Let's see how many new countries we visited per year")
+    st.markdown("Let's see how many **new** countries were visited each year")
     st.altair_chart(new_countries)
 
+    st.markdown("Location by year - interactive map")
+    st.altair_chart(get_visited_map(checkins_df), width=-1)
 
 
 if __name__ == "__main__":
